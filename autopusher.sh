173e7e25d90b9be33bb66055270e15cc409e808e
Arc0r <72944751+Arc0r@users.noreply.github.com>
2025-01-22 07:44:31 +0100

    Create autopusher.sh
    INIT

---
+++ autopusher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Path which will be ignored like /home/user/.local
+EXCLUDE=(".cache" ".local" ".oh-my-zsh" ".cargo")
+for P in "${EXCLUDE[@]}";do
+    EXCLUDEPATH+="-not -path *$P* ";
+done
+
+#Path to search for repositories like /opt/mynewporject/.git/
+SEARCHPATHS="/home /opt"
+for GITPATH in $(find $SEARCHPATHS -name .git $EXCLUDEPATH);do
+    echo "####################################################";
+    cd $GITPATH; cd ..; pwd; 
+    echo "pushing ...";
+    #git commit -a -m "auto commit"; 
+    #git push;
+    git status;
+done
