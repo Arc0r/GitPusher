396311d00acfcf97c12e7cdbdbf72dbd0e1e0158
Paul Müller <junk2me@protonmail.com>
2025-01-22 08:54:59 +0100

    auto commit

--- autopusher.sh
+++ autopusher.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# do while loop
+# ?check for changes?
+
+
+
 #Path which will be ignored like /home/user/.local
 EXCLUDE=(".cache" ".local" ".oh-my-zsh" ".cargo")
 for P in "${EXCLUDE[@]}";do
@@ -6,12 +11,12 @@ for P in "${EXCLUDE[@]}";do
 done
 
 #Path to search for repositories like /opt/mynewporject/.git/
-SEARCHPATHS="/home /opt"
+SEARCHPATHS="/home/pmueller/GitPusher/ "
 for GITPATH in $(find $SEARCHPATHS -name .git $EXCLUDEPATH);do
     echo "####################################################";
     cd $GITPATH; cd ..; pwd; 
     echo "pushing ...";
-    #git commit -a -m "auto commit"; 
-    #git push;
+    git commit -a -m "auto commit"; 
+    git push;
     git status;
 done
